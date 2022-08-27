fa57e1c2c77efdbecc0a82fd09b2da6c9cef5410
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-26 21:52:43 -0300

    notes components created and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import { nanoid } from 'nanoid';
 import { useState } from 'react'
+import NotesList from './components/NotesList';
+import './index.css'
 
 function App() {
 
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "this is my first ever note!",
+      date: '26/08/2022'
+    },
+    {
+      id: nanoid(),
+      text: "this is my second ever note!",
+      date: '27/08/2022'
+    },
+    {
+      id: nanoid(),
+      text: "this is my third ever note!",
+      date: '28/08/2022'
+    },
+  ])
+
   return (
-    <div className="App">
-      teste
+    <div className="container">
+      <NotesList notes={notes} />
     </div>
   )
 }
