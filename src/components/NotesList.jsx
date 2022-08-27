bb1ccccc3082e7569df5996d0f4dacbdffcf459b
Matheus Ferreira <matheus.fer33@gmail.com>
2022-08-26 22:43:03 -0300

    added the delete note functionality

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AddNote from './AddNote'
 import Note from './Note'
 
-const NotesList = ({ notes, handleAddNote }) => {
+const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
   return (
     <div className='notes-list'>
       {notes.map((note) => (
@@ -10,6 +10,7 @@ const NotesList = ({ notes, handleAddNote }) => {
             id={note.id} 
             text={note.text} 
             date={note.date}
+            handleDeleteNote={handleDeleteNote}
          />
       ))}
       <AddNote handleAddNote={handleAddNote} />
